f3ca9899511181081a34930884f2ead425ac01ea
Jonathan Milligan <logouts.deco-0e@icloud.com>
2023-01-13 15:06:06 -0600

    fix: Wrap option

--- convert.py
+++ convert.py
@@ -7,7 +7,7 @@ import markdown2
 parser = argparse.ArgumentParser(description="Convert Markdown to Email")
 parser.add_argument("input", type=argparse.FileType("r"), help="Input file")
 parser.add_argument("-o", "--output", type=argparse.FileType("w"), help="Output file")
-parser.add_argument("-w", "--wrap", type=bool, help="Don't preserve wrapping")
+parser.add_argument("-w", "--wrap", action="store_true", help="Don't preserve wrapping")
 args = parser.parse_args()
 
 # Read the text file
